a340b5f08c071891fd62e50a1e8a0f8c6b39c1d9
FranOttone <Franciscoottone02@gmail.com>
2021-03-27 16:12:57 -0300

    chau

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -1,14 +1,22 @@
-$(function(){
-var autos;
-  $.getJSON("Marcas.json",function(result){
-    $.each(result,function(i,auto){
-      autos+="<option value='"
-      +auto.id+
-      "'>"
-      +auto.nombre+
-      "</option>"; 
-    });
-    $('#marcas').html(autos);
-  });
+var marcas_json;
 
+$(document).ready(function(){
+$.ajax({
+method:"GET",
+url:'https://raw.githubusercontent.com/FranOttone/cursojs/main/Js/Marcas.json'
+}).done(function(data){
+  marcas_json=JSON.parse(data)
+  
+  var selec_marcas= document.createElement('select');
+  selec_marcas.className="selec_marcas";
+
+  for( var i=0; i<marcas_json.marcas.length;i++){
+    var option=document.createElement('option')
+    option.value=i
+    option.text= marcas_json.marcas[i].nombre;
+    option.className='option_'+ i;
+    selec_marcas.appendChild(option)
+  }
+  document.getElementById('selector').appendChild(selec_marcas)
+})
 });
