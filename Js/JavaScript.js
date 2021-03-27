6f0d684d7c10b3383d5a18fc62d31f99c2a0b3c2
FranOttone <Franciscoottone02@gmail.com>
2021-03-27 17:12:40 -0300

    progreso27-3-2021

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -1,3 +1,4 @@
+
 var marcas_json;
 
 $(document).ready(function(){
@@ -20,3 +21,28 @@ url:'https://raw.githubusercontent.com/FranOttone/cursojs/main/Js/Marcas.json'
   document.getElementById('selector').appendChild(selec_marcas)
 })
 });
+var modelosFord_json;
+$(document.ready(function(){
+  $.ajax({
+  method:"GET",
+  url:''   
+  }).done(function(data){
+    modelos_json=JSON.parse(data)
+    
+    if(
+      marcas_json.nombre["Ford"]
+      ){
+        var selec_modelo=document.createElement('select')
+        selec_modelo.className="selec_modelo"
+
+        for(var i=0; i<modelosFord_json.modelos.length;i++){
+          var option=document.createElement('option')
+          option.value=i
+          option.text=modelosFord_json.modelos[i].name;
+          option.className='option_'+ i;
+          selec_modelo.appendChild(option)  
+        }
+        document.getElementById('selector').appendChild(selec_modelo)
+      }
+    })
+  }))
