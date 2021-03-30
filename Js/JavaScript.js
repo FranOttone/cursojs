74eb15f472674940d3ec6c629e5a2bc0043e7d80
FranOttone <Franciscoottone02@gmail.com>
2021-03-29 23:56:59 -0300

    progreso29-3-2021

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -1,47 +1,114 @@
 
-var marcas_json;
 
-$(document).ready(function(){
+var marcas_json;
+$(document).ready(function (){
 $.ajax({
 method:"GET",
-url:'https://raw.githubusercontent.com/FranOttone/cursojs/main/Js/Marcas.json'
+url:'https://gist.githubusercontent.com/AnndresRodriguez/a4216e3f82f45fc4514dc954f967fe9a/raw/a41bf12c2e12cc5e2db88ee30b0d7d3ddaf3f6c2/models.json'
 }).done(function(data){
   marcas_json=JSON.parse(data)
-  
-  var selec_marcas= document.createElement('select');
-  selec_marcas.className="selec_marcas";
+  var selec_modelos=document.getElementById("modelos")
+  $('#marcas').change(function(){
+    console.log($('#marcas').val());
 
-  for( var i=0; i<marcas_json.marcas.length;i++){
+    if($('#marcas').val("1")){
+    for(var i=0;i<marcas_json.marcas.lenght;i++){
+  var option=document.createElement('option')
+  option.value=i
+  option.text=marcas_json.marcas[0].modelos[i]
+  selec_modelos.appendChild(option)
+  }
+  }})
+})
+})
+var marcas_json;
+$(document).ready(function (){
+$.ajax({
+method:"GET",
+url:'https://gist.githubusercontent.com/AnndresRodriguez/a4216e3f82f45fc4514dc954f967fe9a/raw/a41bf12c2e12cc5e2db88ee30b0d7d3ddaf3f6c2/models.json'
+}).done(function(data){
+  marcas_json=JSON.parse(data)
+
+  var selec_marcas=document.getElementById("marcas")
+  var selec_modelos=document.getElementById("modelos")
+  
+  
+    for( var i=0; i<marcas_json.marcas.length;i++){
+      var option=document.createElement('option')
+      option.value=i
+      option.text= marcas_json.marcas[i].nombre;
+      selec_marcas.appendChild(option)
+    }
+if(option.value["0"]){
+  
+  for(var i=0; i<marcas_json.marcas.length;i++){
     var option=document.createElement('option')
     option.value=i
-    option.text= marcas_json.marcas[i].nombre;
-    option.className='option_'+ i;
-    selec_marcas.appendChild(option)
-  }
-  document.getElementById('selector').appendChild(selec_marcas)
-})
-});
-var MODELOS_json;
-
-$(document).ready(function(){
-  $.ajax({
-  method:"GET",
-  url:'https://raw.githubusercontent.com/FranOttone/cursojs/main/Js/Modelos.json'   
-  }).done(function(data){
-    MODELOS_json=JSON.parse(data)
+    option.text=marcas_json.marcas[0].modelos[i];
+    selec_modelos.appendChild(option)
+}
+}
+if(option.value["1"]){
   
-    if(marcas_json.id==1){
-        var selec_modelo=document.createElement('select')
-        selec_modelo.className="selec_modelo"
+  for(var i=0; i<marcas_json.marcas.length;i++){
+    var option=document.createElement('option')
+    option.value=i
+    option.text=marcas_json.marcas[1].modelos;
+    selec_modelos.appendChild(option)
+}}
 
-        for(var i=0; i<MODELOS_json.Modelos[0].length;i++){
-          var option=document.createElement('option')
-          option.value=i
-          option.text=MODELOS_json.modelosFord[i].name;
-          option.className='option_'+ i;
-          selec_modelo.appendChild(option)  
-        }
-        document.getElementById('selector').appendChild(selec_modelo)
-      }
-    })
-  })
+if(option.value["2"]){
+  
+  for(var i=0; i<marcas_json.marcas.length;i++){
+    var option=document.createElement('option')
+    option.value=i
+    option.text=marcas_json.marcas[2].modelos[i];
+    selec_modelos.appendChild(option)
+}}
+
+if(option.value["3"]){
+  
+  for(var i=0; i<marcas_json.marcas.length;i++){
+    var option=document.createElement('option')
+    option.value=i
+    option.text=marcas_json.marcas[3].modelos[i];
+    selec_modelos.appendChild(option)
+}}
+
+if(option.value["4"]){
+  
+  for(var i=0; i<marcas_json.marcas.length;i++){
+    var option=document.createElement('option')
+    option.value=i
+    option.text=marcas_json.marcas[4].modelos[i];
+    selec_modelos.appendChild(option)
+}}
+
+if(option.value["5"]){
+  
+  for(var i=0; i<marcas_json.marcas.length;i++){
+    var option=document.createElement('option')
+    option.value=i
+    option.text=marcas_json.marcas[5].modelos[i];
+    selec_modelos.appendChild(option)
+}}
+
+if(option.value["6"]){
+  
+  for(var i=0; i<marcas_json.marcas.length;i++){
+    var option=document.createElement('option')
+    option.value=i
+    option.text=marcas_json.marcas[6].modelos[i];
+    selec_modelos.appendChild(option)
+}}
+
+if(option.value["7"]){
+  
+  for(var i=0; i<marcas_json.marcas.length;i++){
+    var option=document.createElement('option')
+    option.value=i
+    option.text=marcas_json.marcas[7].modelos[i];
+    selec_modelos.appendChild(option)
+}}
+});
+})
