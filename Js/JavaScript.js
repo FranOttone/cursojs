754fde00c2ec3419733607537aeb4d8b45666458
FranOttone <Franciscoottone02@gmail.com>
2021-04-19 21:48:52 -0300

    Primer-Calculadora

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -1,25 +1,44 @@
+window.onload=calPro();
 
-$(document).ready(function () {
+function calPro(){
+    function Id(id){return document.getElementById(id)}
+    var peso=Id('kg').value
+    var resultado=0
     
-    $('input#kg').change(function()
-    {
-    const peso= $('input#kg').val();
-            console.log(peso) 
-            $('#calcular').click(() =>{
-                $.post("https://jsonplaceholder.typicode.com/posts", peso, function(data, estado) {
-                    var proteinas= parseInt(peso*1.7)
-                    
-                    if(estado ==="success")
-                      {
-                        $('body').append(`<div class="append">Cantidad de proteínas:${proteinas}gr</div>`)
-                
-                      }                      
-                    });
-                  
-
-                });
-                 
-           });
+    if(Id('kg').value !=""  && Id('sedentario').checked &&( Id('hombre').checked|| Id('mujer').checked)){
+            resultado=(peso*1.5)
+            console.log(resultado)
+            $('#creado').remove()
+            $('body').append('<div id="creado" class="append">Proteínas diarias:'+' '+Math.round(resultado)+'</div>')
+            $('.append2').remove()
+            $('.requerido').removeClass('requerido');
+            
+        }
+        else if(Id('kg').value !="" && Id('medianamente').checked && (Id('hombre').checked || Id('mujer').checked)){
+            resultado=(peso*1.6)
+            console.log(resultado)
+            $('#creado').remove()
+            $('body').append('<div id="creado" class="append">Proteínas diarias:'+' '+Math.round(resultado)+'</div>')
+            $('.append2').remove()
+            $('.requerido').removeClass('requerido');
+        }
+        
+        else if(Id('kg').value !="" && Id('activo').checked && (Id('hombre').checked || Id('mujer').checked)){
+            resultado=(peso*1.8)
+            console.log(resultado)
+            $('#creado').remove()
+            $('body').append('<div id="creado" class="append">Proteínas diarias:'+' '+Math.round(resultado)+'</div>')
+            $('.append2').remove()
+            $('.requerido').removeClass('requerido');
+        }
     
-    
-    });
+    else{
+        $('.grid').addClass("requerido");
+        $('.grid1').addClass("requerido");
+        $('.grid2').addClass("requerido");
+        $('.contenedor').append('<div class="append2">*Completa todos los campos*</div>')
+            
+       
+    }
+}
+calPro()
